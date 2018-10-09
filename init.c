e38a5fd8417eb8f0093116b9734ef8345f587b84
mhy <mhyousefi96@gmail.com>
2018-10-09 08:23:02 -0700

    Added other member names. Note Pazki is not misspelled by accident. It is done so to annot Mr Amir Ranjbar!

--- init.c
+++ init.c
@@ -20,7 +20,7 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\nAli Edalat\n");
+    printf(1, "init: starting sh\nAli Edalat\nMohammad Hossein Yousefi\nAmir Pazki\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
