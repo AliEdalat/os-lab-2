c37126d97510f0959c5708b09dc27d49b5474362
AliEdalat <ali.edalat.77@gmail.com>
2018-10-30 19:29:46 +0330

    list of args is problem.

--- proc.c
+++ proc.c
@@ -491,8 +491,9 @@ invocation_log(int pid){
       {
         if (p->syscalls[i].count > 0)
         {
-          cprintf(" syscall : ID :%d NAME:%s \n", i+1,
-              p->syscalls[i].name);
+          cprintf(" syscall : ID :%d NAME:%s DATE: sec:%d min:%d hour:%d day:%d month:%d year:%d\n", i+1,
+            p->syscalls[i].name, p->syscalls[i].date.second, p->syscalls[i].date.minute, p->syscalls[i].date.hour,
+            p->syscalls[i].date.day, p->syscalls[i].date.month, p->syscalls[i].date.year);
            status = 0;
         } 
       }
